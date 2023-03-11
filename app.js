463e8b51ee51fd8e75ea4ae1ce09a8bd56070546
Val10 <valentindragnev101@gmail.com>
2023-03-11 14:48:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 let computerPlay = () => {
     const choices = ["Rock", "Paper", "Scissors"];
-
     const randomChoice = Math.floor(Math.random() * choices.length);
-
     return choices[randomChoice];
 }
 
 let personChoice = () => {
-
     //invoke the computer choice function
     let computerChoice = computerPlay();
     const pcChoice = computerChoice.toLowerCase();
     //playing with the DOM
     const yourChoice = document.getElementById('choice').value.toLowerCase();
-
+    //writing in the DOM
     document.getElementById('PersonChoice').textContent = `Your choice is ${yourChoice}.`;
-
     document.getElementById('ComputerChoice').textContent = `Computer choice is ${pcChoice}.`;
 
     for (let rounds = 1; rounds <= 5; rounds++) {
-        console.log(rounds);
+        // console.log(rounds);
         if (yourChoice === pcChoice) {
             document.getElementById('Winner').textContent = `It's a tie!`;
         } else if (
